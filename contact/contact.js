32f2fd3a673a93e879832eec348c6cf2d5c96cda
Janae Lewis <janaelewis@Janaes-MacBook-Pro.local>
2020-12-04 02:00:57 -0700

    contact form

---
+++ contact/contact.js
@@ -0,0 +1,18 @@
+import{ init } from 'emailjs-com';
+init("user_7tixkLPCLMgwWxnfksQsU");
+
+var templateParams = {
+    name: 'James',
+    notes: 'Check this out!'
+};
+ 
+emailjs.send('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', templateParams)
+    .then(function(response) {
+       console.log('SUCCESS!', response.status, response.text);
+    }, function(error) {
+       console.log('FAILED...', error);
+    });
+
+// (function(){
+//     emailjs.init("YOUR_USER_ID");
+//  })();
